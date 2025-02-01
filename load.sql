ef8e022e0b32ab991f1c51b1e3a9a6dcf230e369
Kuziomkin <51739431+Kuziomkin@users.noreply.github.com>
2025-02-01 11:19:19 +0100

    Add files via upload

---
+++ load.sql
@@ -0,0 +1,1 @@
+COPY fbi_wanted FROM '/content/drive/MyDrive/Colab Notebooks/mydb.db/fbi_wanted.csv' (FORMAT 'csv', quote '"', delimiter ',', header 1);
---
+++ schema.sql
@@ -0,0 +1,8 @@
+
+
+
+CREATE TABLE fbi_wanted(title VARCHAR, description VARCHAR, subject VARCHAR, status VARCHAR, caution VARCHAR, aliaces VARCHAR, reward_text VARCHAR, "publication" DATE, sex VARCHAR, race VARCHAR, hair VARCHAR, height_max DOUBLE, nationality VARCHAR, age_range VARCHAR, weight VARCHAR, dates_of_birth_used VARCHAR, place_of_birth VARCHAR, languages VARCHAR, warning_message VARCHAR, occupations VARCHAR, complexion VARCHAR, reward DOUBLE, age_min BIGINT, age_max BIGINT, subject_group VARCHAR);
+
+
+
+
